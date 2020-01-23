22d281b36dc9fc88a27e338fbcf42f9a082cd6b2
Danila Frolov <tobi-madara-1996@mail.ru>
2020-01-23 11:13:35 +0300

    Added if-else block, for module.parent. Added file app.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 let user = require("./user");
 
-let firstUser = new user.User("Vasya");
-let secondUser = new user.User("Danila");
+function run() {
+    let firstUser = new user.User("Vasya");
+    let secondUser = new user.User("Danila");
 
-firstUser.hello(secondUser);
-secondUser.hello(firstUser);
+    firstUser.hello(secondUser);
+    secondUser.hello(firstUser);
+}
+
+if (module.parent) {
+    exports.run = run;
+} else {
+    run();
+}
