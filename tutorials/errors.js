e82622160a18ab1f8d34d90f36f2f7b4dc9a22e5
Danila Frolov <tobi-madara-1996@mail.ru>
2020-01-23 16:37:59 +0300

    added simple example of errors processing

---
+++ tutorials/errors.js
@@ -0,0 +1,48 @@
+var util = require('util');
+var phrases = { "Hello": "Привет", "World": "Мир" };
+
+//all default erros contains name of error
+
+function PhraseError(message) {
+    this.message = message;
+    //collect stack of this code part; 
+    //second argument - ignore this function
+    Error.captureStackTrace(this, PhraseError);
+}
+
+util.inherits(PhraseError, Error);
+PhraseError.prototype.name = "PhraseError";
+
+function HttpError(status, message) {
+    this.status = status;
+    this.message = message;
+    Error.captureStackTrace(this, HttpError);
+}
+
+util.inherits(HttpError, Error);
+HttpError.prototype.name = "HttpError";
+
+function getPhrases(name) {
+    if (!phrases[name]) {
+        throw new PhraseError("Phrase: " + name + " - does not exist");
+    }
+    return phrases[name];
+}
+
+function makePage(url) {
+    if (url != 'index.html') {
+        throw new HttpError(404, "Page: " + url + " - does not found");
+    }
+    return util.format("%s, %s!", getPhrases("Hello"), getPhrases("World"));
+}
+
+try {
+    var page = makePage('index.html');
+    console.log(page);
+} catch (e) {
+    if (e instanceof HttpError) {
+        console.log(e.status, e.message);
+    } else {
+        console.error("Error: %s\n message: %s\n stack: %s", e.name, e.message, e.stack);
+    }
+}
